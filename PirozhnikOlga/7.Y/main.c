30d20157df994f5b1b2836aed0447d2f63cfc408
Harald Zealot <harald_zealot@tut.by>
2014-12-21 17:39:39 +0300

    pirozxnik notes

--- PirozhnikOlga/7.Y/main.c
+++ PirozhnikOlga/7.Y/main.c
@@ -5,21 +5,25 @@ int main()
 {
     int chislo, seredina, n, k, str, i;
     printf("Vvedite nechetnoe chislo:\n");
-    scanf("%d"), &chislo;
-    while(chislo%2==0)
+    scanf("%d", &chislo);//тут была памылка
+
+    while(chislo % 2 == 0)
     {
         printf("Ne verno!\n");
         printf("Vvedite nechetnoe chislo:\n");
         scanf("%d", &chislo);
     }
-    seredina=chislo/2;
-    k=chislo;
-    n=1;
-    for(str=1; str<seredina;++str)
+
+    seredina = chislo / 2;
+    k = chislo;
+    n = 1;    k = chislo;
+
+
+    for(str = 1; str <= seredina; ++str)//тут крыху памежныя умовы іншыя
     {
-        for(i=1; i<=chislo; ++i)
+        for(i = 1; i <= chislo; ++i)
         {
-            if(i==n||i==k)
+            if(i == n || i == k)
             {
                 printf("Y");
             }
@@ -28,29 +32,55 @@ int main()
                 printf(" ");
             }
         }
+
         printf("\n");
-        i=1;
-        --n;
-        ++k;
+        i = 1;
+        ++n;//а тут наадварот было
+        --k;// і тут
     }
-    n=seredina-1/2;
-    for(i=1; i<=chislo; ++i)
+
+    //n = seredina - 1 / 2;//непатрэбны радок
+
+    //тут перамудрылі
+    //вось такі варыянт працуе:
+    /*
+    for(; str <= chislo; ++str)
+    {
+        for(i = 1; i <= n; ++i)
         {
-            for(str; str<n; ++str)
+            if(i == n)
             {
-                for(i<chislo)
-                {
-                    if(i==n)
-                    {
-                        printf("Y");
-                    }
-                    else
-                    {
-                        printf(" ");
-                    }
-                }
-                printf("\n");
+                printf("Y");
+            }
+            else
+            {
+                printf(" ");
             }
         }
+
+        printf("\n");
+    }
+    /*/
+    for(i = 1; i <= chislo; ++i)
+    {
+        printf("str = %d, n = %d\n", str, n);
+        for(str; str < n; ++str)
+        {
+            while(i < chislo) //а тут напісалі for
+            {
+                if(i == n)
+                {
+                    printf("Y");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+
+            printf("\n");
+        }
+    }
+
     return 0;
 }
